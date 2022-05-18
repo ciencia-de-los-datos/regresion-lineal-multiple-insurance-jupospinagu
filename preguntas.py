13db9d45b016e1400b19ec0b1bee5463a6706434
Juan Pablo Ospina Gutierrez <101611634+jupospinagu@users.noreply.github.com>
2022-05-17 19:17:11 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -90,7 +90,7 @@ def pregunta_03():
                         OneHotEncoder(),
                         make_column_selector(dtype_include= object),
                     ),
-                    remainder='drop',
+                    remainder='passthrough',
                 ),
             ),
             # Paso 2: Construya un selector de características que seleccione las K
